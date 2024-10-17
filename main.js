a2315eb79bc1cf6ee133b5425f38b5b1a93e84f6
unknown <maima.alive25@gmail.com>
2024-10-17 22:31:00 +0500

    my first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// i++ postfix increment
+for (var i = 0; i < 5; i++) {
+    console.log("world is temorary", i);
+}
+// ++i prefix increment
+for (var j = 0; j < 5; ++j) {
+    console.log("there are 8 planet in our solar system", j);
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+// i++ postfix increment
+for(let i=0; i<5; i++){
+    console.log("world is temorary", i)
+}
+
+// ++i prefix increment
+for(let j=0; j<5; ++j){
+    console.log("there are 8 planet in our solar system", j)
+}
